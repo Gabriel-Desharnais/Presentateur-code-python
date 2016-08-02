25ab3760e600e937ea2faf2ebcf6fdc269e0da42
gabriel <gabriel.desharnais@hotmail.com>
2016-08-02 18:40:05 -0400

    le truc pour modifier

--- cgi/mod.py
+++ cgi/mod.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*
 
 import cgi 
-
+from gogogadjet import *
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
 print("Vous êtes en train de modifier la présentation",form.getvalue("id"))
+page=gogogadjet("mod.html")
+
+print(page)
